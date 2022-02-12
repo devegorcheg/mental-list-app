4fa750a78e1937d877c5a9a8cd0969640a35dd9d
Egor <egorcheg1@gmail.com>
2022-02-13 00:54:52 +0300

    WIP(app): 🚧 add Logo

--- src/auth/components/Wrapper.tsx
+++ src/auth/components/Wrapper.tsx
@@ -1,19 +1,24 @@
 // component
 import { Box, Container, Paper } from "@mui/material";
+import { Logo } from "./Icons/Logo";
 
 // utils
-import { styled } from "@mui/system";
+import { styled, SxProps, Theme } from "@mui/system";
 
-const sxWapper = { display: "flex", minHeight: "100vh" };
-const sxBackground = {
-  background: "no-repeat url(auth-bg.png)",
+const sxWapper: SxProps<Theme> = { display: "flex", minHeight: "100vh" };
+const sxBackground: SxProps<Theme> = {
+  background: "no-repeat url(imgs/auth-bg.png)",
   backgroundSize: "cover",
   flexGrow: 0,
   flexBasis: "47%",
   maxWidth: "47%",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
 };
 
-const AuthBox = styled(Box)(() => ({
+const SAuthBox = styled(Box)(() => ({
   display: "flex",
   flexDirection: "column",
   flexBasis: "53%",
@@ -32,11 +37,17 @@ const AuthBox = styled(Box)(() => ({
   },
 }));
 
+const SLogo = styled(Logo)(({ theme: { spacing } }: { theme: Theme }) => ({
+  paddingBottom: spacing(6.25),
+}));
+
 export const Wrapper: React.FC = ({ children }) => {
   return (
     <Box sx={sxWapper}>
-      <Box sx={sxBackground} />
-      <AuthBox>
+      <Box sx={sxBackground}>
+        <SLogo width="39%" />
+      </Box>
+      <SAuthBox>
         <Container maxWidth="sm">
           <Paper elevation={5}>
             <Box px={5} py={5}>
@@ -44,7 +55,7 @@ export const Wrapper: React.FC = ({ children }) => {
             </Box>
           </Paper>
         </Container>
-      </AuthBox>
+      </SAuthBox>
     </Box>
   );
 };
