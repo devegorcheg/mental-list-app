54ee1dd71a88044298808d8607a4ad0b69f97da1
Egor <egorcheg1@gmail.com>
2022-02-13 19:09:41 +0300

    WIP(app): 🚧 change primery.main and text.secondary colors

--- src/lib/mui.ts
+++ src/lib/mui.ts
@@ -1,10 +1,12 @@
 import { createTheme } from "@mui/material/styles";
-import { deepPurple } from "@mui/material/colors";
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: deepPurple[500],
+      main: "#2F80ED",
+    },
+    text: {
+      secondary: "#BBBBC7",
     },
   },
   typography: {
