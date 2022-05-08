76c187b99b7f9291aade9fba573be03051a138f6
Egor <egorcheg1@gmail.com>
2022-05-09 00:19:08 +0300

    WIP(tasks): 🚧 add TimePickerButton

---
+++ src/common/components/TimePickerButton.tsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+
+// components
+import { IconButton, Popover, SxProps, TextField, Theme } from "@mui/material";
+import { StaticTimePicker } from "@mui/x-date-pickers/StaticTimePicker";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+
+// utils
+import { styled } from "@mui/material/styles";
+
+interface Props {
+  value: Date | null;
+  onChange: (date: Date | null) => void;
+  sx?: SxProps<Theme>;
+}
+
+const SIconButton = styled(IconButton, {
+  shouldForwardProp: propName => propName !== "open",
+})<{ open?: boolean }>(({ theme, open }) => ({
+  ...(open && { color: theme.palette.primary.main }),
+}));
+
+export const TimePickerButton: React.FC<Props> = ({ value, onChange, sx }) => {
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(anchorEl => (anchorEl ? null : event.currentTarget));
+  };
+
+  return (
+    <>
+      <SIconButton
+        onClick={handleClick}
+        size="small"
+        open={open}
+        sx={sx}
+        aria-describedby="time-picker-popover"
+      >
+        <AccessTimeIcon />
+      </SIconButton>
+
+      <Popover
+        id="time-picker-popover"
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClick}
+        PaperProps={{ sx: { height: "372px", overflow: "hidden" } }}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+        transformOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+      >
+        <StaticTimePicker
+          displayStaticWrapperAs="mobile"
+          value={value}
+          onChange={onChange}
+          renderInput={params => <TextField {...params} />}
+        />
+      </Popover>
+    </>
+  );
+};
--- src/tasks/components/AddTask/AddTaskActions.tsx
+++ src/tasks/components/AddTask/AddTaskActions.tsx
@@ -3,6 +3,7 @@ import { Field, FieldProps } from "formik";
 // components
 import { Box, Button } from "@mui/material";
 import { DatePickerButton } from "common/components/DatePickerButton";
+import { TimePickerButton } from "common/components/TimePickerButton";
 
 export const AddTaskActions: React.FC = () => {
   return (
@@ -10,10 +11,18 @@ export const AddTaskActions: React.FC = () => {
       <Box display="flex">
         <Field name="dueDate">
           {({ field, form }: FieldProps<Date>) => (
-            <DatePickerButton
-              value={field.value}
-              onChange={date => form.setFieldValue("dueDate", date)}
-            />
+            <Box mx={-1}>
+              <DatePickerButton
+                value={field.value}
+                onChange={date => form.setFieldValue("dueDate", date)}
+                sx={{ marginX: 1 }}
+              />
+              <TimePickerButton
+                value={field.value}
+                onChange={date => form.setFieldValue("dueDate", date)}
+                sx={{ marginX: 1 }}
+              />
+            </Box>
           )}
         </Field>
       </Box>
