f72131cbe3b3c7dea449130465c8d8671b714349
Egor <egorcheg1@gmail.com>
2022-05-08 23:27:16 +0300

    WIP(tasks): 🚧 rename ToDoPage

--- src/tasks/components/TasksPage.tsx
+++ src/tasks/components/TasksPage.tsx
@@ -2,11 +2,13 @@
 import { Box } from "@mui/material";
 
 import { MainMenu } from "common/components/MainMenu";
+import { AddTask } from "./AddTask";
 
-export const ToDoPage: React.FC = () => {
+export const TasksPage: React.FC = () => {
   return (
     <Box sx={{ position: "relative" }}>
       <MainMenu sx={{ position: "absolute", top: 26, right: 26 }} />
+      <AddTask />
     </Box>
   );
 };
